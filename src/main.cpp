5a17a97d0856098c38f04437a8ebce8d4a407e8b
Jaydeep Chakrabarty <jaydeepc@in-Jaydeepc-3089.local>
2021-05-12 00:56:35 +0530

    JD/Dheeraj: Added BLE starter and notify code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Keypad.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLECharacteristic *pCharacteristic;
 
 const byte ROWS = 8; //four rows
 const byte COLS = 8; //three columns
@@ -30,10 +39,12 @@ Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 unsigned long loopCount;
 unsigned long startTime;
 String msg;
+void startBLE();
 
 void setup()
 {
   Serial.begin(9600);
+  startBLE();
 }
 
 void loop()
@@ -43,6 +54,33 @@ void loop()
   {
     int index = (int)key;
     String cell = cells[index - 1];
+    pCharacteristic->setValue(cell.c_str());
+    pCharacteristic->notify();
     Serial.println(cell);
   }
+}
+
+void startBLE(){
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Rolling Pawn");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE|
+                                         BLECharacteristic::PROPERTY_NOTIFY
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
