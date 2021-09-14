2db2ccf01b941391f33c7c87dd4553c42eea9312
Jaydeep Chakrabarty <jaydeepc@in-Jaydeepc-3089.local>
2021-09-15 00:22:35 +0530

    JD/Atul/Kanishka: Fixed ios bluetooth issue

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -113,10 +114,11 @@ void startBLE()
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
       CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
+         BLECharacteristic::PROPERTY_READ |
           BLECharacteristic::PROPERTY_WRITE |
           BLECharacteristic::PROPERTY_NOTIFY);
 
+  pCharacteristic->addDescriptor(new BLE2902());
   pCharacteristic->setValue("Hello World says Neil");
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
